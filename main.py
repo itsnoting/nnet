9854061e51eaa60f166f35ed83cde4a8c6a9d1b4
Kevin Ting <kting93@gmail.com>
2015-11-16 21:52:34 -0800

    Printing stats

--- main.py
+++ main.py
@@ -19,17 +19,34 @@ def matrix_to_list(matrix):
 
 def main():
 
+    #Training set initiated
     trainer_set = ubyte_unpack("./ubyte", "training")
     inputs, solutions = trainer_set.get_dataset(100)
 
     NN = Neural_Network()
+
+    print "TRAINING CASES"
+
+    #solutions = matrix_to_list(solutions)
+    #Running with training cases before training
+    yHat = matrix_to_list(NN.forward(inputs))
+    before_solutions = matrix_to_list(solutions)
+    percent = pred_perc(before_solutions, yHat)
+    print "Prediction percentage before training:", percent * 100
+
+    #Training Neural Network
     T = trainer(NN)
     T.train(inputs, solutions)
+
+    #Running with training cases after training
     yHat = matrix_to_list(NN.forward(inputs))
     solutions = matrix_to_list(solutions)
     percent = pred_perc(solutions, yHat)
-    print "Prediction percentage:", percent * 100
+    print "Prediction percentage after training:", percent * 100
 
+    print "\nTESTING CASES"
+
+    #Running with testing cases after training
     tester_set = ubyte_unpack("./ubyte", "testing")
     X, y = tester_set.get_dataset(1000)
 
@@ -37,7 +54,7 @@ def main():
     y = matrix_to_list(y)
     test_percent = pred_perc(y, test_yHat)
     print "Test prediction percentage:", test_percent * 100
-    print "Current first level weights:", NN.W1
+    print "\n\nCurrent first level weights:", NN.W1
     print "Current second level weights:", NN.W2
 
 if __name__ == "__main__":
